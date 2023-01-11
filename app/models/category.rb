2d7982bd9cbd7ecfad8fc8bfb69f2fefd78f2e03
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-01-11 11:13:08 +0000

    Add navbar

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
-  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  validates :name, presence: true, length: { minimum: 3 }
 
+
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :expenses_categories, foreign_key: 'category_id'
   has_many :expenses, through: :expenses_categories
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :categories, foreign_key: 'author_id'
   has_many :expenses, foreign_key: 'author_id'
 
-  validates :name, presence: true , length: {minimum: 3, maximum: 25 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
+  # validates :email, presence: true:, uniquesess: { case_sensitive: false}
 end
